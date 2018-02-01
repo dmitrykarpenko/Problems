6a77a87cadc1b142f456fe15b195b84fdfac9928
Dmytro Karpenko <dmitry.karpenko@gmail.com>
2018-02-02 01:04:35 +0200

    PatternMatcher logic and test added

---
+++ Problems.Domain.Tests/Logic/Strings/PatternMatcherTest.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Problems.Domain.Logic.Strings.PatternMatcher;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.Domain.Tests.Logic.Strings
+{
+    [TestClass]
+    public class PatternMatcherTest
+    {
+        [TestMethod]
+        public void IsMatchTrueTest()
+        {
+            //Arrange:
+            IPatternMatcher patternMatcher = new SplittingPatternMatcher();
+
+            var pairs = new[]
+            {
+                // TODO: optimize performance
+                //new
+                //{
+                //    Input = "abbabaaabbabbaababbabbbbbabbbabbbabaaaaababababbbabababaabbababaabbbbbbaaaabababbbaabbbbaabbbbababababbaabbaababaabbbababababbbbaaabbbbbabaaaabbababbbbaababaabbababbbbbababbbabaaaaaaaabbbbbaabaaababaaaabb",
+                //    Pattern = "**aa*****ba*a*bb**aa*ab****a*aaaaaa***a*aaaa**bbabb*b*b**aaaaaaaaa*a********ba*bbb***a*ba*bb*bb**a*b*bb"
+                //},
+                new { Input = "abb", Pattern = "**??" },
+                new { Input = "aaaa", Pattern = "***a" },
+                new { Input = "", Pattern = "" },
+                new { Input = "", Pattern = "*" },
+                new { Input = "abcdefgh", Pattern = "ab?d*g?" },
+                new { Input = "abcdefgh", Pattern = "a**g?" },
+                new { Input = "abcdefgh", Pattern = "a*bcdefgh" },
+                new { Input = "abcdefgh", Pattern = "abcdefg*h" },
+                new { Input = "abcdefgh", Pattern = "*abcdefgh*" },
+                new { Input = "abcdefgh", Pattern = "*a*" },
+                new { Input = "abcdefgh", Pattern = "*d*" },
+                new { Input = "abcdefgh", Pattern = "*h*" },
+                new { Input = "abcdefgh", Pattern = "*????????*" },
+                new { Input = "abcdefgh", Pattern = "?bcdefg?" },
+                new { Input = "abcdefgh", Pattern = "abcd?fgh" },
+                new { Input = "abcdefgh", Pattern = "a*h" },
+                new { Input = "abcdefgh", Pattern = "a***h" },
+                new { Input = "abcdefgh", Pattern = "a**************h" },
+                new { Input = "abcdefgh", Pattern = "ab***gh" },
+                new { Input = "abcdefgh", Pattern = "*ab***gh*" },
+                new { Input = "abcdefgh", Pattern = "*a*b*c*d*e*f*g*h*" },
+                new { Input = "abcdefgh", Pattern = "**a***b**c**d***e**f**g**h**" },
+                new { Input = "?bcdefgh", Pattern = "?bcdefgh" },
+                new { Input = "abcdefg?", Pattern = "abcdefg?" },
+                new { Input = "abc?efg?", Pattern = "?bc?efg?" },
+                new { Input = "?bc?efg?", Pattern = "*bc?efg*" },
+                new { Input = "*", Pattern = "*" },
+                new { Input = "*", Pattern = "***" },
+                new { Input = "*", Pattern = "?" },
+                new { Input = "?", Pattern = "?" },
+                new { Input = "?*?", Pattern = "*" },
+                new { Input = "?*?", Pattern = "*****" },
+                new { Input = "??", Pattern = "*" },
+                new { Input = "abc**efgh", Pattern = "abc??efgh" },
+            };
+
+            foreach (var pair in pairs)
+            {
+                //Act:
+                var isMatch = patternMatcher.IsMatch(pair.Input, pair.Pattern);
+
+                //Assert:
+                Assert.IsTrue(isMatch, $"{nameof(pair.Input)} : '{pair.Input}' {nameof(pair.Pattern)} : '{pair.Pattern}'");
+            }
+        }
+        [TestMethod]
+        public void IsMatchFalseTest()
+        {
+            //Arrange:
+            IPatternMatcher patternMatcher = new SplittingPatternMatcher();
+
+            var pairs = new[]
+            {
+                new { Input = "*", Pattern = "" },
+                new { Input = "?", Pattern = "" },
+                new { Input = "", Pattern = "?" },
+                new { Input = "", Pattern = "???" },
+                new { Input = "??", Pattern = "???" },
+                new { Input = "???", Pattern = "??" },
+                new { Input = "abcdefgh", Pattern = "abcdefhg" },
+                new { Input = "abcdefgh", Pattern = "??????hg" },
+                new { Input = "abcdefgh", Pattern = "???????gh" },
+                new { Input = "abcdefgh", Pattern = "????????hg" },
+                new { Input = "abcdefgh", Pattern = "ab?d*g?h" },
+                new { Input = "abcdefgh", Pattern = "*g" },
+                new { Input = "abcdefgh", Pattern = "*f" },
+                new { Input = "abcdefgh", Pattern = "a**g" },
+                new { Input = "abcdefgh", Pattern = "*a*g" },
+                new { Input = "abcdefgh", Pattern = "b*" },
+                new { Input = "abcdefgh", Pattern = "b*g*" },
+            };
+
+            foreach (var pair in pairs)
+            {
+                //Act:
+                var isMatch = patternMatcher.IsMatch(pair.Input, pair.Pattern);
+
+                //Assert:
+                Assert.IsFalse(isMatch, $"{nameof(pair.Input)} : '{pair.Input}' {nameof(pair.Pattern)} : '{pair.Pattern}'");
+            }
+        }
+    }
+}
---
+++ Problems.Domain/Logic/Strings/PatternMatcher/SplittingPatternMatcher.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Problems.Domain.Logic.Strings.PatternMatcher
+{
+    public class SplittingPatternMatcher : IPatternMatcher
+    {
+        private const char sequenceWildcard = '*';
+        private const char charWildcard = '?';
+
+        public bool IsMatch(string input, string pattern)
+        {
+            if (input == null || pattern == null)
+                throw new ArgumentNullException();
+            if (input == pattern)
+                return true;
+            if (input != string.Empty && pattern == string.Empty)
+                return false;
+
+            var patternParts = pattern.Split(sequenceWildcard);
+            patternParts = RemoveEmpty(patternParts);
+
+            // pattern only contains sequenceWildcards
+            if (pattern != string.Empty && patternParts.Length == 0)
+                return true;
+
+            var anyStart = pattern[0] == sequenceWildcard;
+            var anyEnd = pattern[pattern.Length - 1] == sequenceWildcard;
+
+            return IsMatchRecursive(input, 0,
+                patternParts, 0,
+                anyStart, anyEnd);
+        }
+
+        /// <param name="patternParts">each patternPart will not contain sequenceWildcard (*) - only charWildcard (?)</param>
+        private static bool IsMatchRecursive(string input, int firstInputCharIndex,
+            string[] patternParts, int firstPatternPartIndex, 
+            bool anyStart, bool anyEnd)
+        {
+            if (firstPatternPartIndex == patternParts.Length)
+                return anyEnd || firstInputCharIndex == input.Length;
+
+            var firstPatternPart = patternParts[firstPatternPartIndex];
+
+            var indices = FindIndices(input, firstInputCharIndex, firstPatternPart, anyStart ? int.MaxValue : 1);
+
+            foreach (var index in indices)
+            {
+                if (!anyStart && index > firstInputCharIndex)
+                    return false;
+
+                var isMatch = IsMatchRecursive(input, index + firstPatternPart.Length,
+                    patternParts, firstPatternPartIndex + 1,
+                    true, anyEnd);
+
+                if (isMatch)
+                    return true;
+            }
+
+            return false;
+        }
+
+        private static int[] FindIndices(string input, int firstInputCharIndex, string patternPart, int maxIndicesCount)
+        {
+            var indices = new List<int>();
+            for (var inputCharIndex = firstInputCharIndex; inputCharIndex < input.Length; ++inputCharIndex)
+            {
+                if (IsExactMatch(input, inputCharIndex, patternPart))
+                    indices.Add(inputCharIndex);
+                if (indices.Count == maxIndicesCount)
+                    return indices.ToArray();
+            }
+            return indices.ToArray();
+        }
+
+        private static bool IsExactMatch(string input, int firstInputCharIndex, string patternPart)
+        {
+            var nextAfterLastInputCharIndex = firstInputCharIndex + patternPart.Length;
+            if (nextAfterLastInputCharIndex > input.Length)
+                return false;
+
+            for (var inputCharIndex = firstInputCharIndex; inputCharIndex < nextAfterLastInputCharIndex; ++inputCharIndex)
+            {
+                var patternIndex = inputCharIndex - firstInputCharIndex;
+                if (patternPart[patternIndex] != input[inputCharIndex] &&
+                    patternPart[patternIndex] != charWildcard)
+                    return false;
+            }
+
+            return true;
+        }
+
+        // usually it's done with Linq - implemented just to be less language-specific
+        private static string[] RemoveEmpty(string[] patternParts)
+        {
+            var nonEmptyPatternParts = new List<string>();
+            for (int i = 0; i < patternParts.Length; i++)
+                if (!string.IsNullOrEmpty(patternParts[i]))
+                    nonEmptyPatternParts.Add(patternParts[i]);
+            
+            return nonEmptyPatternParts.ToArray();
+        }
+    }
+}
