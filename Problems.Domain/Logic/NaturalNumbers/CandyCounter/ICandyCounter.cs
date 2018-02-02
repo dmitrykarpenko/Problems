0ee367bc4607fae5fae95c1426d3320eaca21efd
Dmytro Karpenko <dmitry.karpenko@gmail.com>
2018-02-02 18:56:58 +0200

    ICandyCounter, IPatternMatcher - summaries added

--- Problems.Domain/Logic/NaturalNumbers/CandyCounter/ICandyCounter.cs
+++ Problems.Domain/Logic/NaturalNumbers/CandyCounter/ICandyCounter.cs
@@ -6,6 +6,13 @@ using System.Threading.Tasks;
 
 namespace Problems.Domain.Logic.NaturalNumbers.CandyCounter
 {
+    /// <summary>
+    /// There are N children standing in a line. Each child is assigned a rating value.
+    /// You are giving candies to these children subjected to the following requirements:
+    /// Each child must have at least one candy.
+    /// Children with a higher rating get more candies than their neighbors.
+    /// What is the minimum candies you must give?
+    /// </summary>
     public interface ICandyCounter
     {
         int Candy(int[] ratings);
--- Problems.Domain/Logic/Strings/PatternMatcher/IPatternMatcher.cs
+++ Problems.Domain/Logic/Strings/PatternMatcher/IPatternMatcher.cs
@@ -6,6 +6,22 @@ using System.Threading.Tasks;
 
 namespace Problems.Domain.Logic.Strings.PatternMatcher
 {
+    /// <summary>
+    /// Implement wildcard pattern matching with support for '?' and '*'.
+    /// '?' Matches any single character.
+    /// '*' Matches any sequence of characters (including the empty sequence).
+    /// The matching should cover the entire input string (not partial).
+    /// The function prototype should be:
+    /// bool isMatch(const char* s, const char* p)
+    /// Some examples:
+    /// isMatch("aa","a") → false
+    /// isMatch("aa","aa") → true
+    /// isMatch("aaa","aa") → false
+    /// isMatch("aa", "*") → true
+    /// isMatch("aa", "a*") → true
+    /// isMatch("ab", "?*") → true
+    /// isMatch("aab", "c*a*b") → false
+    /// </summary>
     public interface IPatternMatcher
     {
         bool IsMatch(string input, string pattern);
