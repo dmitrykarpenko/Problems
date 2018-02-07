e57d12fe913f46d43a2c15c455710d2681bf80f2
Dmytro Karpenko <dmytro.karpenko@strikersoft.com>
2018-02-07 15:33:12 +0200

    DecodeOptionsCounter - initial

---
+++ Problems.Domain.Tests/Logic/NaturalNumbers/DecodeOptionsCounterTest.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Problems.Domain.Logic.Strings.DecodeOptionsCounter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.Domain.Tests.Logic.NaturalNumbers
+{
+    [TestClass]
+    public class DecodeOptionsCounterTest
+    {
+        [TestMethod]
+        public void NumDecodingsTest()
+        {
+            // Arrange:
+            IDecodeOptionsCounter decodeOptionsCounter = new RecursiveDecodeOptionsCounter();
+
+            var inputObjects = new[]
+            {
+                new
+                {
+                    Input = "ABAB",
+                    Output = 5,
+                },
+                new
+                {
+                    Input = "AB",
+                    Output = 2,
+                },
+            };
+
+            foreach (var inputObject in inputObjects)
+            {
+                // Act:
+                var output = decodeOptionsCounter.NumDecodings(inputObject.Input);
+
+                // Assert:
+                Assert.AreEqual(inputObject.Output, output,
+                    $"{inputObject.Input} should have {inputObject.Output} ways of decoding");
+            }
+        }
+    }
+}
---
+++ Problems.Domain/Logic/Strings/DecodeOptionsCounter/RecursiveDecodeOptionsCounter.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.Domain.Logic.Strings.DecodeOptionsCounter
+{
+    public class RecursiveDecodeOptionsCounter : IDecodeOptionsCounter
+    {
+        public int NumDecodings(string s)
+        {
+            _minCharCode = GetCharCode('A');
+            _maxCharCode = GetCharCode('Z');
+            _maxCharCodeFirstDigit = _maxCharCode / 10;
+
+            return CountDecodeOptions(s);
+        }
+
+        private int _minCharCode;
+        private int _maxCharCode;
+
+        private int _maxCharCodeFirstDigit;
+
+        private Dictionary<string, int> _optionCounts = new Dictionary<string, int>();
+
+        private int CountDecodeOptions(string input)
+        {
+            if (input == null || input.Length == 0)
+                return 1;
+            if (input.Length == 1)
+                return 1;
+
+            var firstCharCode = GetCharCode(input[0]);
+            var secondCharCode = GetCharCode(input[1]);
+
+            var number = firstCharCode * 10 + secondCharCode;
+            if (number > _maxCharCode)
+            {
+                var nextInput = input.Substring(2);
+
+                // * 1 - because have only one option to decode two first letters
+                return GetOrAddCount(nextInput);
+            }
+
+            return GetOrAddCount(input.Substring(1)) + GetOrAddCount(input.Substring(2));
+        }
+
+        private int GetOrAddCount(string input)
+        {
+            int count;
+            if (!_optionCounts.TryGetValue(input, out count))
+            {
+                count = CountDecodeOptions(input);
+                _optionCounts.Add(input, count);
+            }
+            return count;
+        }
+
+        private static int GetCharCode(char c)
+        {
+            return (int)c - (int)('A') + 1;
+        }
+    }
+}
