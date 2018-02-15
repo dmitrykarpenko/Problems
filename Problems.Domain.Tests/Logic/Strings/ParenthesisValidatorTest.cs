5fd806c82bc6405999409ac4137d17fd85b9032c
Dmytro Karpenko <dmytro.karpenko@strikersoft.com>
2018-02-15 19:34:16 +0200

    ParenthesisValidator - initial

---
+++ Problems.Domain.Tests/Logic/Strings/ParenthesisValidatorTest.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Problems.Domain.Logic.Strings.ParenthesisValidator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.Domain.Tests.Logic.Strings
+{
+    [TestClass]
+    public class ParenthesisValidatorTest
+    {
+        [TestMethod]
+        public void CheckValidStringTest()
+        {
+            // Arrange:
+            IParenthesisValidator parenthesisValidator = new SequentialParenthesisValidator();
+
+            var inputObjects = new[]
+            {
+                //new { Input = "(())((())()()(*)(*()(())())())()()((()())((()))(*", Output = false },
+                new { Input = "", Output = true },
+                new { Input = "()", Output = true },
+                new { Input = "((*))", Output = true },
+                new { Input = "(*))", Output = true },
+                new { Input = "(*))", Output = true },
+                new { Input = "(*()", Output = true },
+                new { Input = "(*()", Output = true },
+                new { Input = "*))", Output = false },
+                new { Input = ")*()", Output = false },
+                new { Input = Create(('(', 1000), ('*', 900), (')', 900)), Output = true },
+                new { Input = Create(('(', 1000), ('*', 100), (')', 900)), Output = true },
+                new { Input = Create(('*', 1000)), Output = true },
+                new { Input = Create(('(', 1000), ('*', 99), (')', 900)), Output = false },
+            };
+
+            foreach (var inputObject in inputObjects)
+            {
+                // Act:
+                var output = parenthesisValidator.CheckValidString(inputObject.Input);
+
+                // Assert:
+                Assert.AreEqual(inputObject.Output, output);
+            }
+        }
+
+        private static string Create(params (char, int)[] chars) => Create((IEnumerable<(char, int)>)chars);
+        private static string Create(IEnumerable<(char, int)> chars) => new string(CreateEnumerable(chars).ToArray());
+
+        private static IEnumerable<char> CreateEnumerable(IEnumerable<(char, int)> chars)
+        {
+            foreach (var c in chars)
+                for (int i = 0; i < c.Item2; ++i)
+                    yield return c.Item1;
+        }
+    }
+}
---
+++ Problems.Domain/Logic/Strings/ParenthesisValidator/SequentialParenthesisValidator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.Domain.Logic.Strings.ParenthesisValidator
+{
+    public class SequentialParenthesisValidator : IParenthesisValidator
+    {
+        public bool CheckValidString(string s)
+        {
+            return AreParenthesisValid(s);
+        }
+
+        private static bool AreParenthesisValid(string s)
+        {
+            int parenthesisCount = 0,
+                wildcardCount = 0;
+            for (int i = 0; i < s.Length; ++i)
+            {
+                if (s[i] == '(')
+                    ++parenthesisCount;
+                else if (s[i] == ')')
+                    --parenthesisCount;
+                else if (s[i] == '*')
+                    ++wildcardCount;
+
+                // if too many right or too many left, return false
+                if (parenthesisCount + wildcardCount < 0 ||
+                    parenthesisCount - wildcardCount > s.Length - 1 - i)
+                    return false;
+            }
+
+            return wildcardCount - Math.Abs(parenthesisCount) > 0;
+        }
+    }
+}
