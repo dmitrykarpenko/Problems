30eace71331c985351aab85e183c2358fbfc9f67
Dmytro Karpenko <dmytro.karpenko@strikersoft.com>
2018-02-16 13:08:32 +0200

    SpecialBinaryStringSorter - interface and test

---
+++ Problems.Domain.Tests/Logic/NaturalNumbers/SpecialBinaryStringSorterTest.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Problems.Domain.Logic.NaturalNumbers.SpecialBinaryStringSorter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.Domain.Tests.Logic.NaturalNumbers
+{
+    [TestClass]
+    public class SpecialBinaryStringSorterTest
+    {
+        [TestMethod]
+        public void MakeLargestSpecialTest()
+        {
+            // Arrange:
+            ISpecialBinaryStringSorter specialBinaryStringSorter = new SimpleSpecialBinaryStringSorter();
+
+            var inputObjects = new[]
+            {
+                new { Input = "11011000", Output = "11100100" },
+            };
+
+            foreach (var inputObject in inputObjects)
+            {
+                //// Act:
+                //var output = specialBinaryStringSorter.MakeLargestSpecial(inputObject.Input);
+
+                //// Assert:
+                //Assert.AreEqual(inputObject.Output, output);
+            }
+        }
+    }
+}
---
+++ Problems.Domain/Logic/NaturalNumbers/SpecialBinaryStringSorter/SimpleSpecialBinaryStringSorter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.Domain.Logic.NaturalNumbers.SpecialBinaryStringSorter
+{
+    public class SimpleSpecialBinaryStringSorter : ISpecialBinaryStringSorter
+    {
+        public string MakeLargestSpecial(string S)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
