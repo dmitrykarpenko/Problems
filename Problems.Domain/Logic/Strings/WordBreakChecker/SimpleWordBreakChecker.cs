0d12b5cbcf6f8e7b762be9074591c84f6917086e
Dmytro Karpenko <dmytro.karpenko@strikersoft.com>
2018-02-19 18:19:59 +0200

    SimpleWordBreakChecker - initial

--- Problems.Domain/Logic/Strings/WordBreakChecker/SimpleWordBreakChecker.cs
+++ Problems.Domain/Logic/Strings/WordBreakChecker/SimpleWordBreakChecker.cs
@@ -8,9 +8,61 @@ namespace Problems.Domain.Logic.Strings.WordBreakChecker
 {
     public class SimpleWordBreakChecker : IWordBreakChecker
     {
+		private readonly Dictionary<int, Dictionary<string, WordInfo>> _words =
+			new Dictionary<int, Dictionary<string, WordInfo>>();
+		
         public bool WordBreak(string s, IList<string> wordDict)
         {
-            throw new NotImplementedException();
+			InitializeWords(wordDict);
+            return IsWordBreakPossible(s);
         }
+		
+		private class WordInfo
+		{
+			public string Word;
+			public bool IsUsed;
+		}
+		
+		private bool IsWordBreakPossible(string input)
+		{
+			for (int i = 1; i < input.Length; ++i)
+			{
+				//...
+			}
+            return false;
+		}
+		
+		private bool FindBeginning(string input, int length, out string word)
+		{
+			word = input.Substring(0, length);
+			
+			Dictionary<string, WordInfo> current;
+			if (! _words.TryGetValue(length, out current))
+				return false;
+
+			WordInfo wordInfo;
+			if (! current.TryGetValue(word, out wordInfo))
+				return false;
+			
+			wordInfo.IsUsed = true;
+			return true;
+		}
+		
+		private void InitializeWords(IList<string> words)
+		{
+			foreach (var word in words)
+			{
+				Dictionary<string, WordInfo> current;
+				if (_words.TryGetValue(word.Length, out current))
+				{
+					current.Add(word, new WordInfo { Word = word, IsUsed = false });
+				}
+				else
+				{
+					_words[word.Length] = new Dictionary<string, WordInfo>
+						{ { word, new WordInfo { Word = word, IsUsed = false } } };
+				}
+			}
+		}
     }
 }
