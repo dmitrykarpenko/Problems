cc1dc3fcd19827fee5e0b3b5d490d07b2214a7f8
Dmytro Karpenko <dmytro.karpenko@strikersoft.com>
2018-03-12 13:14:08 +0200

    ListSorter - interface added

---
+++ Problems.Domain/Logic/Collections/ListSorter/IListSorter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.Domain.Logic.Collections.ListSorter
+{
+    /// <summary>
+    /// Definition for singly-linked list.
+    /// </summary>
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
+    }
+
+    public interface IListSorter
+    {
+        /// <summary>
+        /// Sort a linked list in O(n log n) time using constant space complexity.
+        /// </summary>
+        /// <param name="head">list's head node</param>
+        /// <returns></returns>
+        ListNode SortList(ListNode head);
+    }
+}
