81ce46e60cd7860887c6023df26b97786f59a105
Dmytro Karpenko <dmytro.karpenko@strikersoft.com>
2018-04-24 12:38:45 +0300

    DelegateTest with ClosureTest added

---
+++ Problems.Domain.Tests/Logic/Performance/DelegateTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.Domain.Tests.Logic.Performance
+{
+    [TestClass]
+    public class DelegateTest
+    {
+        [TestMethod]
+        public void ClosureTest()
+        {
+            Action action, nestedAction1, nestedAction2;
+            nestedAction1 = () => { };
+            nestedAction2 = () => { };
+            action = () =>
+            {
+                nestedAction2();
+            };
+            action();
+        }
+    }
+}
