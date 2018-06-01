68878dd897726331c622ddd0fb2f877c5532cd24
Dmytro Karpenko <dmytro.karpenko@strikersoft.com>
2018-06-01 15:52:56 +0300

    LargestHistogramRectangularFinder - submitted solution

---
+++ Problems.Domain.Tests/Logic/Matrices/LargestHistogramRectangularFinderTest.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Problems.Domain.Logic.Matrices.LargestHistogramRectangularFinder;
+using Problems.Domain.Tests.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.Domain.Tests.Logic.Matrices
+{
+    [TestClass]
+    public class LargestHistogramRectangularFinderTest
+    {
+        [TestMethod]
+        public void LargestHistogramRectangularFinder_LargestRectangleArea_Test()
+        {
+            ILargestHistogramRectangularFinder largestHistogramRectangularFinder = new LargestHistogramRectangularFinder();
+
+            // Arrange:
+            var inputObjects = new[]
+            {
+                new { Input = new[] { 2, 1, 5, 6, 2, 3 }, Output = 10, },
+                new { Input = new[] { 2, 1, 5, 6, 2, 3, 10 }, Output = 10, },
+                new { Input = new[] { 2, 1, 5, 6, 5 }, Output = 15, },
+                new { Input = new[] { 2 }, Output = 2, },
+                new { Input = new int[0], Output = 0, },
+                new { Input = (int[])null, Output = 0, },
+                new { Input = GenericUtil.CreateArray((2, 100), (3, 200)), Output = 2*(100 + 200), },
+                new { Input = GenericUtil.CreateArrayFromEnumerables((new[] { 2, 0 }, 100)), Output = 2*1, },
+            };
+
+            foreach (var inputObject in inputObjects)
+            {
+                // Act:
+                var output = largestHistogramRectangularFinder.LargestRectangleArea(inputObject.Input);
+
+                // Assert:
+                Assert.AreEqual(inputObject.Output, output);
+            }
+        }
+    }
+}
--- Problems.Domain/Logic/Matrices/LargestHistogramRectangularFinder/LargestHistogramRectangularFinder.cs
+++ Problems.Domain/Logic/Matrices/LargestHistogramRectangularFinder/LargestHistogramRectangularFinder.cs
@@ -1,5 +1,4 @@
-﻿using Problems.Domain.Logic.Matrices.LargetsHistogramRectangularFinder;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,9 +8,91 @@ namespace Problems.Domain.Logic.Matrices.LargestHistogramRectangularFinder
 {
     public class LargestHistogramRectangularFinder : ILargestHistogramRectangularFinder
     {
+        /// <summary>
+        /// Finds maximum rectangular area under a given histogram.
+        /// Time Complexity: O(n) since every bar is pushed and popped only once. 
+        /// </summary>
+        /// <param name="_heights">The given histogram</param>
+        /// <returns></returns>
         public int LargestRectangleArea(int[] heights)
         {
-            throw new NotImplementedException();
+            if (heights == null)
+                return 0;
+
+            Initialize(heights);
+
+            // Run through all bars of the given histogram
+            while (_i < _heights.Length)
+            {
+                if (!_indices.Any() || _heights[_indices.Peek()] <= _heights[_i])
+                {
+                    // if this bar is higher than the bar on the top of the stack, push it to the stack
+                    _indices.Push(_i++);
+                }
+                else
+                {
+                    // else, if this bar is lower than the top of the stack, calculate the area of the rectangle 
+                    // with the stack top as the smallest (or "minimum height") bar.
+                    PopIndexAndCalculateArea();
+                }
+            }
+
+            // Now pop the remaining bars from stack and calculate area with every
+            // popped bar as the smallest bar
+            while (_indices.Any())
+            {
+                PopIndexAndCalculateArea();
+            }
+
+            return _maxArea;
+        }
+
+        /// <summary> The given histogram </summary>
+        private int[] _heights;
+        /// <summary> 
+        /// The stack holds indices of <see cref="_heights"/> array
+        /// The bars stored in the stack are always in
+        /// increasing order of their heights
+        /// (would have been popped otherwise).
+        /// </summary>
+        private Stack<int> _indices;
+        /// <summary> The current index </summary>
+        private int _i;
+        /// <summary> The wanted max area </summary>
+        private int _maxArea;
+        /// <summary> Top of the stack </summary>
+        private int _indicesTop;
+        /// <summary> Area with the top bar as the smallest bar </summary>
+        private int _areaWithTop;
+
+        private void Initialize(int[] heights)
+        {
+            _heights = heights;
+            _indices = new Stack<int>();
+            _i = 0;
+            _maxArea = 0;
+        }
+
+        private void PopIndexAndCalculateArea()
+        {
+            // _i is the 'right index' for the top and the element before the top in the stack is the 'left index'
+
+            // Store the top index and POP the top:
+            _indicesTop = _indices.Pop();
+
+            // CALCULATE the area with heights[indicesTop] stack as the smallest bar:
+
+            var areaHeight = _heights[_indicesTop];
+            var areaWidth = !_indices.Any()
+                ? _i // no _indices, so the area "goes" right to the start of the histogram
+                : _i - _indices.Peek() - 1; // have an index, so the area "stops" before the index 
+                                            // (i.e. "stops" at the popped _indicesTop index)
+
+            _areaWithTop = areaHeight * areaWidth;
+
+            // Update max area, if needed:
+            if (_maxArea < _areaWithTop)
+                _maxArea = _areaWithTop;
         }
     }
 }
