cda0023acd3a015f1a80ef250a273e06a8179ef0
Dmytro Karpenko <dmytro.karpenko@strikersoft.com>
2018-06-05 14:10:08 +0300

    ISorter - Sort - summary added

--- Problems.Domain/Logic/Collections/SortingAlgorithms/ISorter.cs
+++ Problems.Domain/Logic/Collections/SortingAlgorithms/ISorter.cs
@@ -5,6 +5,12 @@ namespace Problems.Domain.Logic.Collections.SortingAlgorithms
 {
     public interface ISorter
     {
+        /// <summary>
+        /// Sorts items in-place.
+        /// </summary>
+        /// <typeparam name="T">Type of an item, should be an IComparable of T </typeparam>
+        /// <param name="items">Array to sort</param>
+        /// <param name="desc">False - ascending, true - descending</param>
         void Sort<T>(IList<T> items, bool desc = false) where T : IComparable<T>;
     }
 }
