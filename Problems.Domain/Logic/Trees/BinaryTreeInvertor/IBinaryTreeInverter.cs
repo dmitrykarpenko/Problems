17ac37556a897d99adb79808889bbea64a1e9147
Dmytro Karpenko <dmytro.karpenko@strikersoft.com>
2018-06-07 12:30:30 +0300

    BinaryTreeInverter - an interface and submitted solution added

---
+++ Problems.Domain/Logic/Trees/BinaryTreeInvertor/IBinaryTreeInverter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.Domain.Logic.Trees.BinaryTreeInvertor
+{
+    public interface IBinaryTreeInverter
+    {
+        /// <summary>
+        /// https://leetcode.com/problems/invert-binary-tree/description/
+        /// 226. Invert Binary Tree
+        /// 
+        /// Invert a binary tree.
+        /// 
+        /// Example:
+        /// 
+        /// Input:
+        /// 
+        ///      4
+        ///    /   \
+        ///   2     7
+        ///  / \   / \
+        /// 1   3 6   9
+        /// 
+        /// Output:
+        /// 
+        ///      4
+        ///    /   \
+        ///   7     2
+        ///  / \   / \
+        /// 9   6 3   1
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        TreeNode InvertTree(TreeNode root);
+    }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+    }
+}
---
+++ Problems.Domain/Logic/Trees/BinaryTreeInvertor/RecursiveBinaryTreeInverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.Domain.Logic.Trees.BinaryTreeInvertor
+{
+    public class RecursiveBinaryTreeInverter : IBinaryTreeInverter
+    {
+        public TreeNode InvertTree(TreeNode root)
+        {
+            if (root == null)
+                return null;
+
+            SwapChildren(root);
+
+            InvertTree(root.left);
+            InvertTree(root.right);
+
+            return root;
+        }
+
+        private static void SwapChildren(TreeNode node)
+        {
+            TreeNode temp = node.left;
+            node.left = node.right;
+            node.right = temp;
+        }
+    }
+}
