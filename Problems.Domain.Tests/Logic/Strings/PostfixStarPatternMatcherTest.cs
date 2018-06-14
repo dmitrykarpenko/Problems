1908b25ae4c8d39ca9ccd6e23879a01d06913ef1
Dmytro Karpenko <dmytro.karpenko@strikersoft.com>
2018-06-14 18:11:53 +0300

    PostfixStarPatternMatcher - smoke test passed

---
+++ Problems.Domain.Tests/Logic/Strings/PostfixStarPatternMatcherTest.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Problems.Domain.Logic.Strings.PostfixStarPatternMatcher;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.Domain.Tests.Logic.Strings
+{
+    [TestClass]
+    public class PostfixStarPatternMatcherTest
+    {
+        [TestMethod]
+        public void PostfixStarPatternMatcher_IsMatch_Test()
+        {
+            //Arrange:
+            IPostfixStarPatternMatcher patternMatcher = new PostfixStarPatternMatcher();
+
+            var inputObjects = new[]
+            {
+                new { Input = "", Pattern = "", Output = true },
+                new { Input = "aa", Pattern = "aa", Output = true },
+                new { Input = "", Pattern = "a*", Output = true },
+                new { Input = "bb", Pattern = "bb", Output = true },
+                new { Input = "bb", Pattern = "bb", Output = true },
+                new { Input = "aba", Pattern = "a.a", Output = true },
+                new { Input = "abbb", Pattern = "ab*", Output = true },
+                new { Input = "acd", Pattern = "ab*c.", Output = true },
+                new { Input = "abaa", Pattern = "a.*a*", Output = true },
+                new { Input = "aa", Pattern = "a*", Output = true },
+                new { Input = "ab", Pattern = ".*", Output = true },
+                new { Input = "aab", Pattern = "c*a*b", Output = true },
+
+                new { Input = "aa", Pattern = "a", Output = false },
+                new { Input = "abbdbb", Pattern = "ab*d", Output = false },
+                new { Input = "mississippi", Pattern = "mis*is*p*.", Output = false },
+                new { Input = "a", Pattern = ".*..a*", Output = false },
+            };
+
+            foreach (var inputObject in inputObjects)
+            {
+                //Act:
+                var output = patternMatcher.IsMatch(inputObject.Input, inputObject.Pattern);
+
+                //Assert:
+                Assert.AreEqual(inputObject.Output, output,
+                    $@"{nameof(inputObject.Input)} : '{inputObject.Input
+                    }' {nameof(inputObject.Pattern)} : '{inputObject.Pattern}'");
+            }
+        }
+    }
+}
---
+++ Problems.Domain/Logic/Strings/PostfixStarPatternMatcher/PostfixStarPatternMatcher.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.Domain.Logic.Strings.PostfixStarPatternMatcher
+{
+    public class PostfixStarPatternMatcher : IPostfixStarPatternMatcher, ILcPostfixStarPatternMatcher
+    {
+        public bool IsMatch(string text, string pattern)
+        {
+            if (pattern.Length == 0)
+            {
+                return text.Length == 0;
+            }
+
+            if (pattern.Length == 1)
+            {
+                return pattern[0] == '.' && text.Length == 1 || pattern == text;
+            }
+
+            if (pattern[1] == '*')
+            {
+                // pattern starts with ...
+                // _*
+
+                if (text.Length > 0)
+                {
+                    if (pattern[0] == '.')
+                    {
+                        // .*
+
+                        // heve we have the ".*" (i.e. "catch'em all") pattern part,
+                        // so we either remove the current letter form text
+                        // or remove the ".*" part and try to 
+                        // here's where exponential complexity emerges
+                        return GocIsMatch(text.Substring(1), pattern) ||
+                               GocIsMatch(text, pattern.Substring(2));
+                    }
+                    else if (text[0] == pattern[0])
+                    {
+                        // a*
+
+                        // thus, remove first a form text
+                        return GocIsMatch(text.Substring(1), pattern);
+                    }
+                }
+
+                // _*
+
+                // didn't match, so should be removed
+                return GocIsMatch(text, pattern.Substring(2));
+            }
+
+            // .b or ab
+
+            // remove one char from both pattern and text
+            return text.Length > 0 && (pattern[0] == '.' || text[0] == pattern[0]) &&
+                   GocIsMatch(text.Substring(1), pattern.Substring(1));
+        }
+
+        private Dictionary<Tuple<string, string>, bool> _isMatches =
+            new Dictionary<Tuple<string, string>, bool>();
+
+        // Get or calculate IsMatch
+        private bool GocIsMatch(string text, string pattern)
+        {
+            var key = new Tuple<string, string>(text, pattern);
+            bool isMatch;
+            if (_isMatches.TryGetValue(key, out isMatch))
+            {
+                return isMatch;
+            }
+
+            return _isMatches[key] = IsMatch(text, pattern);
+        }
+    }
+}
