00151aa9f467a2e5bdfa010c0adce30f0ad2c2b0
Dmytro Karpenko <dmytro.karpenko@strikersoft.com>
2018-06-15 13:42:06 +0300

    PostfixStarPatternMatcher - substrings removed, common code refacrored out - submitted solution

--- Problems.Domain.Tests/Logic/Strings/PostfixStarPatternMatcherTest.cs
+++ Problems.Domain.Tests/Logic/Strings/PostfixStarPatternMatcherTest.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Problems.Domain.Logic.Strings.PostfixStarPatternMatcher;
+using Problems.Domain.Tests.Utils;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,6 +18,7 @@ namespace Problems.Domain.Tests.Logic.Strings
             //Arrange:
             IPostfixStarPatternMatcher patternMatcher = new PostfixStarPatternMatcher();
 
+            var largeAxbc = GenericUtil.CreateString(('a', 100), ('x', 2), ('b', 200), ('c', 300));
             var inputObjects = new[]
             {
                 new { Input = "", Pattern = "", Output = true },
@@ -29,13 +31,26 @@ namespace Problems.Domain.Tests.Logic.Strings
                 new { Input = "acd", Pattern = "ab*c.", Output = true },
                 new { Input = "abaa", Pattern = "a.*a*", Output = true },
                 new { Input = "aa", Pattern = "a*", Output = true },
+                new { Input = "aa", Pattern = "a*a", Output = true },
                 new { Input = "ab", Pattern = ".*", Output = true },
                 new { Input = "aab", Pattern = "c*a*b", Output = true },
+                new { Input = "aaaaaabbbbbbzzzbbbacccccc", Pattern = "a*b*.*.cccc*", Output = true },
+                new { Input = "aaaabbbbzzbbbacccccc", Pattern = ".*", Output = true },
+                new { Input = largeAxbc, Pattern = ".*", Output = true },
+                new { Input = largeAxbc, Pattern = "..*..", Output = true },
+                new { Input = largeAxbc, Pattern = "a.*c", Output = true },
+                new { Input = largeAxbc, Pattern = "a*..b*c*", Output = true },
 
                 new { Input = "aa", Pattern = "a", Output = false },
                 new { Input = "abbdbb", Pattern = "ab*d", Output = false },
                 new { Input = "mississippi", Pattern = "mis*is*p*.", Output = false },
                 new { Input = "a", Pattern = ".*..a*", Output = false },
+                new { Input = "aaaabbbbzzbbbacccccc", Pattern = "a*b*.*.cccc*y", Output = false },
+                new { Input = "aaaabbbbzzbbbacccccc", Pattern = "a*b*.*.ccfcc*", Output = false },
+                new { Input = "aaaabbbbzzbbbacccccc", Pattern = "z.*", Output = false },
+                new { Input = largeAxbc, Pattern = "a*.b*c*", Output = false },
+                new { Input = largeAxbc, Pattern = "a*b*c*", Output = false },
+                new { Input = largeAxbc, Pattern = ".*x", Output = false },
             };
 
             foreach (var inputObject in inputObjects)
