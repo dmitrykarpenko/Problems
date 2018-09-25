81e83de77d6d7b93ea9adf92f162ebd0859e6a54
Dmytro Karpenko <dmitry.karpenko@gmail.com>
2018-09-26 00:48:51 +0300

    LinqTest - GetIntsWithForeach_Test added, common code refactored out, test outputs added

--- Problems.Domain.Tests/Logic/Performance/LinqTest.cs
+++ Problems.Domain.Tests/Logic/Performance/LinqTest.cs
@@ -13,6 +13,12 @@ namespace Problems.Domain.Tests.Logic.Performance
     {
         public TestContext TestContext { get; set; }
 
+        //[TestInitialize]
+        //public void Initialize()
+        //{
+
+        //}
+
         [TestMethod]
         public void ToArrayVsToList_Scale1Count100_Test() =>
             ToArrayVsToListTest(1, 100);
@@ -43,42 +49,108 @@ namespace Problems.Domain.Tests.Logic.Performance
             //AssertUtil.AssertRoughlyEqual(toArrayResult.TimeSpent, toListResult.TimeSpent, 5);
         }
 
-        private const int _count = 105;
+        [TestMethod]
+        public void GetIntsWithTake_Test() =>
+            GetIntsTest((ints, batchSize, processCurrentInts) =>
+            {
+                IReadOnlyCollection<int> currentInts;
+                // WARNING: "Take" starts from the beginngin each time
+                while ((currentInts = ints.Take(batchSize).ToArray()).Count > 0)
+                {
+                    processCurrentInts(currentInts);
+
+                    ints = ints.Skip(batchSize);
+                }
+            });
 
         [TestMethod]
-        public void GetIntsWithTake_Test()
+        public void GetIntsWithForeach_Test() =>
+            GetIntsTest((ints, batchSize, processCurrentInts) =>
+            {
+                List<int> currentInts = new List<int>();
+                foreach (var i in ints)
+                {
+                    currentInts.Add(i);
+                    if (currentInts.Count >= batchSize)
+                    {
+                        processCurrentInts(currentInts);
+
+                        currentInts.Clear();
+                    }
+                }
+
+                if (currentInts.Count > 0)
+                {
+                    processCurrentInts(currentInts);
+                }
+            });
+
+        private int _yieldsCount;
+        private List<int> _yieldedIs;
+
+        private const int _count = 105;
+
+        private void GetIntsTest(
+            Action<IEnumerable<int>, int, Action<IReadOnlyCollection<int>>> processInts)
         {
             // Arrange:
+            _yieldsCount = 0;
+            _yieldedIs = new List<int>();
+
             var ints = GetInts();
 
-            var batchSize = 10;
+            const int batchSize = 10;
 
-            IReadOnlyCollection<int> currentInts;
             int count = 0, sum = 0;
 
             // Act:
-            while ((currentInts = ints.Take(batchSize).ToArray()).Count > 0)
-            {
-                count += currentInts.Count;
-                foreach (var i in currentInts)
+            processInts(
+                ints, batchSize,
+                currentInts =>
                 {
-                    sum += i;
-                }
-
-                ints = ints.Skip(batchSize);
-            }
+                    {
+                        count += currentInts.Count;
+                        foreach (var currentInt in currentInts)
+                        {
+                            sum += currentInt;
+                        }
+                    }
+                });
 
             // Assert:
+            TestContext.WriteLine("yielded i(s): " + string.Join(", ", _yieldedIs));
+            TestContext.WriteLine($"Looped through {nameof(GetInts)} iterrator: {_yieldsCount} times");
+
             Assert.AreEqual(_count, count);
 
             var properSum = GetInts().Sum();
             Assert.AreEqual(properSum, sum);
         }
 
-        private static IEnumerable<int> GetInts()
+        private static void ProcessIntsWithForeach(
+            IEnumerable<int> ints, int batchSize,
+            Action<IReadOnlyCollection<int>> processCurrentInts)
+        {
+            List<int> currentInts = new List<int>();
+            foreach (var i in ints)
+            {
+                currentInts.Add(i);
+                if (currentInts.Count >= batchSize)
+                {
+                    processCurrentInts(currentInts);
+                    currentInts.Clear();
+                }
+            }
+        }
+
+        private IEnumerable<int> GetInts()
         {
             for (int i = 0; i < _count; i++)
             {
+                ++_yieldsCount;
+                System.Diagnostics.Debug.WriteLine($"i: {i}");
+
+                _yieldedIs.Add(i);
                 yield return i;
             }
         }
