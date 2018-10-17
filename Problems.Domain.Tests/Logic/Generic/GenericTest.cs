c4cbc9f0427be6c5377e9eb162bd94351fbc29bb
dmitrykarpenko <dmitry.karpenko@gmail.com>
2018-10-17 12:29:01 +0300

    GenericTest - EqualityComparerDefault_Test added

--- Problems.Domain.Tests/Logic/Generic/GenericTest.cs
+++ Problems.Domain.Tests/Logic/Generic/GenericTest.cs
@@ -35,5 +35,41 @@ namespace Problems.Domain.Tests.Logic.Generic
                 Assert.AreEqual(inputObject.Output, output);
             }
         }
+
+        [TestMethod]
+        public void EqualityComparerDefault_Test()
+        {
+            // Arrange, act, assert:
+
+            Assert.AreEqual(true, Equals(1, 1));
+            Assert.AreEqual(true, Equals(100_000_000_000, 100_000_000_000));
+            Assert.AreEqual(true, Equals(100_000_000_000, (double)100_000_000_000));
+            Assert.AreEqual(true, Equals((long?)null, (double?)null));
+            Assert.AreEqual(true, Equals("one", "one"));
+            Assert.AreEqual(true, Equals(null, null));
+            Assert.AreEqual(true, Equals(new Tuple<string, short>("seventeen", 42), new Tuple<string, short>("seventeen", 42)));
+            Assert.AreEqual(true, Equals(new Tuple<int, int>(17, 42), new Tuple<int, int>(17, 42)));
+            Assert.AreEqual(true, Equals((17, 42), (17, 42)));
+            Assert.AreEqual(true, Equals(new { Number = 52 }, new { Number = 52 }));
+
+            Assert.AreEqual(false, Equals(1, 2));
+            Assert.AreEqual(false, Equals("one", "one1"));
+            Assert.AreEqual(false, Equals(new object(), new object()));
+            Assert.AreEqual(false, Equals(new Tuple<int, int>(17, 42), new Tuple<long, short>(17, 42)));
+            Assert.AreEqual(false, Equals(new Tuple<int, int>(17, 42), new Tuple<int, int>(17, 43)));
+            Assert.AreEqual(false, Equals(new Tuple<int, int>(17, 42), new Tuple<int, int>(18, 42)));
+            Assert.AreEqual(false, Equals((17, 42), (17, 43)));
+            Assert.AreEqual(false, Equals((17, 42), (18, 42)));
+            Assert.AreEqual(false, Equals(new { Number = 52 }, new { Number = 53 }));
+            Assert.AreEqual(false, Equals(new { Number = 52 }, new { Num = 52 }));
+
+            var oldTuple = new Tuple<int, int>(17, 42);
+            var newTuple = (17, 42);
+            // sad but false:
+            Assert.AreEqual(false, Equals(oldTuple, newTuple));
+        }
+
+        private static bool Equals<T>(T first, T second) =>
+            EqualityComparer<T>.Default.Equals(first, second);
     }
 }
